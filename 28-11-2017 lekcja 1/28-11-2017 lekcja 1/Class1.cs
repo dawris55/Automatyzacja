2849bfceec1942e866e36fe14b7c61b53fbbcc8b
dawris55 <dawid.riss@gmail.com>
2017-11-28 11:25:17 +0100

    cokolwiekv2

--- 28-11-2017 lekcja 1/28-11-2017 lekcja 1/Class1.cs
+++ 28-11-2017 lekcja 1/28-11-2017 lekcja 1/Class1.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace _28_11_2017_lekcja_1
 {
-    public class Class1
+    public class Mathematics
     {
+        public double Add (double x, double y)
+            {
+            return x + y;
+            }
+        public double substract (double x, double y)
+            {
+            return x - y;
+            }
+        public double divide (double x, double y)
+            {
+            return x * y;
+            }
+        public double multiply (double x, double y)
+            {
+            return x / y;
+            }
     }
+
 }
