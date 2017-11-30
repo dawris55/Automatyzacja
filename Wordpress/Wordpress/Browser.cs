23d3738837fe16c1b7c0cdcd5d50b78d7356af1b
dawris55 <dawid.riss@gmail.com>
2017-11-30 17:31:20 +0100

    Nie dziala, wordpress usunal pole password

---
+++ Wordpress/Wordpress/Browser.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Wordpress
+{
+    internal class Browser
+    {
+        private static IWebDriver driver;
+        internal void WaitForClickable(By by, int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+        internal static IWebElement FindElementById(string id)
+        {
+            return driver.FindElement(By.Id(id));
+        }
+
+        static Browser()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage()
+                .Window
+                .Maximize();
+            driver.Manage()
+                .Timeouts()
+                .ImplicitWait = TimeSpan.FromMilliseconds(500);
+        }
+
+        internal static string ReturnPageSource()
+        {
+            string pagesource = driver.PageSource;
+            return pagesource;
+        }
+
+        internal static ReadOnlyCollection<IWebElement> FindByXpath(string xpath)
+        {
+            return driver.FindElements(By.XPath(xpath));
+        }
+
+        internal static void NavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+        internal static void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Wordpress/Wordpress/Class1.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Wordpress
+{
+    public class Wordpress
+    {
+
+        public class AddingBlogCommentsTests : IDisposable
+        {
+            private static string url = "https://autotestdotnet.wordpress.com/wp-admin/";
+            private static string user = "autotestdotnet@gmail.com";
+            private static string pass = "P@ssw0rd1";
+            private static string title = Guid.NewGuid().ToString();
+            private static string content = Guid.NewGuid().ToString();
+            [Fact]
+            public void LogingIn()
+            {
+                Browser.NavigateTo(url);
+                var logInField = Browser.FindByXpath("//*[@name='usernameOrEmail']").First();
+                logInField.Click();
+                logInField.SendKeys(user);
+                var passField = Browser.FindByXpath("//*[@name='password']").First();
+                passField.Click();
+                passField.SendKeys(pass);
+                var LogInButon = Browser.FindByXpath("//button[contains(text(),'Log In')]").First();
+                LogInButon.Click();
+                Thread.Sleep(3000);
+                var Pagesbutton = Browser.FindByXpath("//*[@class='wp-menu-name' and text()='Posts']").First();
+                Pagesbutton.Click();
+                Thread.Sleep(3000);
+                var AddNew = Browser.FindByXpath("//*[@class='page-title-action' and text()='Add New']").First();
+                AddNew.Click();
+                var TitleOfNote = Browser.FindElementById("title-prompt-text");
+                TitleOfNote.Click();
+                TitleOfNote.SendKeys(title);
+                var ContentOfNote = Browser.FindElementById("content");
+                ContentOfNote.Click();
+                ContentOfNote.SendKeys(title);
+                //var EditButton = Browser.FindByXpath("//button[contains(text(),'Edit')]").First();
+                //EditButton.Browser.WaitForClickable(By.LinkText("Edit"), 11);
+                
+
+            }
+
+            public void Dispose()
+            {
+                Browser.Close();
+            }
+        }
+    }
+}
